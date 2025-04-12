3ec7cd1793cc2e9598404b10f33b5bd7bf3cff77
pink10000 <kyletrinh1223@gmail.com>
2025-04-11 20:52:24 -0700

    fix light mode pink

--- quartz/quartz.config.ts
+++ quartz/quartz.config.ts
@@ -34,7 +34,7 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#dd1399",
+          secondary: "#ed74c3",
           tertiary: "#5a6ded",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#fff23688",
